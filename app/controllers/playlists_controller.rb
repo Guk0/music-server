d14c46ff4d3c20e9bd0df14430edf69eba36299a
guk0 <abcgy39@naver.com>
2022-09-18 18:48:42 +0900

    update playlists_controller so that user can handle only my_album type playlist

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,18 +1,18 @@
 class PlaylistsController < ApplicationController
-  before_action :load_owner
-  before_action :load_playlist, only: [:show, :update, :destroy]
+  before_action :load_owner, only: [:create, :update, :destroy]
+  before_action :load_playlist, only: [:update, :destroy]
 
   def index
-    playlists = @owner.playlists.where(list_type: params[:list_type] || "default").page(params[:page]).per(10)
+    playlists = Playlist.my_album.page(params[:page]).per(10)
     render json: PlaylistBlueprint.render(playlists)
   end
   
-  def show # tracks과 함께 보여줘야함.
-    render json: PlaylistBlueprint.render(@playlist, view: :detail)
+  def show
+    playlist = Playlist.my_album.find(params[:id])
+    render json: PlaylistBlueprint.render(playlist, view: :detail)
   end
 
   def create
-    # owner can create only my_album type in this action.
     playlist = @owner.playlists.my_album.create(playlist_params)
     render json: PlaylistBlueprint.render(playlist)
   end
@@ -36,7 +36,7 @@ class PlaylistsController < ApplicationController
   end
 
   def load_playlist
-    @playlist = @owner.playlists.find_by(id: params[:id])
+    @playlist = @owner.playlists.my_album.find_by(id: params[:id])
   end
   
   def playlist_params

eb4e6655b69615d38aba7dadba778fa13363d874
guk0 <abcgy39@naver.com>
2022-09-19 01:09:13 +0900

    change

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # Create elastic search index. Returns nil and doesn't create index if indexes already exists.
-# Track.__elasticsearch__.create_index!
+Track.__elasticsearch__.create_index!
 
 seed_class = GenerateSeed.new()
 # seed_class.generate_artists
 # seed_class.generate_albums
 # seed_class.generate_tracks
-# seed_class.generate_artists_albums_tracks
+seed_class.generate_artists_albums_tracks
 seed_class.generate_user
 seed_class.generate_group
 seed_class.generate_playlist
